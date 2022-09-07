41f79d7f79b6446dae121ec3d97345ae965e2555
Geovanna Bamberg <geovanna.t.bamberg@gmail.com>
2022-09-07 17:07:39 -0300

    2° dia
    colocando especificações nos inputs

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 formularioHTML()
 function formularioHTML() {
-    let elementoSection=document.querySelector("section, .formulario")
+    let elementoSection=document.querySelector("form, .formulario")
     elementoSection.innerHTML= `<label>Nome Completo</label>
-    <input type="text" name="nomeCompleto" id="nomeUsuario">
+    <input type="text" name="nomeCompleto" id="nomeUsuario" maxlength="120" minlength="3" required>
     <br>`
     elementoSection.innerHTML+= `<label>Data De Nascimento</label>
-    <input type="date" name="dataNascimento" id="dataNascimento">
+    <input type="date" name="dataNascimento" id="dataNascimento" required>
     <br>`
     elementoSection.innerHTML+= `<label>Email</label>
-    <input type="email" name="email " id="email">
+    <input type="email" name="email " id="email" required>
     <br>`
     elementoSection.innerHTML+= `<label>Comprovação de Email</label>
-    <input type="email" name="ConfirmEmail " id="ConfirmEmail">
+    <input type="email" name="ConfirmEmail " id="ConfirmEmail" required>
     <br>`
     elementoSection.innerHTML+= `<label>Senha</label>
-    <input type="password" name="senha" id="senha">
+    <input type="password" name="senha" id="senha" required>
     <br>`
     elementoSection.innerHTML+= `<label>Comfirmação de Senha</label>
-    <input type="password" name="ConfirmSenha" id="ConfirmSenha">
+    <input type="password" name="ConfirmSenha" id="ConfirmSenha" required minlength="10">
     <br>`
     elementoSection.innerHTML+= `
-    <button onclick="cadastrar()">Cadastrar</button>
+    <button type="submit" onclick="cadastrar()">Cadastrar</button>
     <br>` 
 }
 function cadastrar(){
@@ -31,7 +31,7 @@ function cadastrar(){
     const elementoSenha= document.getElementById("senha").value
     const elementoConfirmSenha= document.getElementById("ConfirmSenha").value
 
-    if ((elementoNome||elementoDataNascimento||elementoConfirmEmail||elementoEmail||elementoSenha||elementoConfirmSenha)=="") {
+    if ((elementoNome&&elementoDataNascimento&&elementoConfirmEmail&&elementoEmail&&elementoSenha&&elementoConfirmSenha)=="") {
         alert("[ERRO] cadastro incompleto")
     } else {
         alert("tudo certo")
